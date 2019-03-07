5c2ea1177166c18e5a6f00289f1909daaeee59e5
idaliri <idaliri69@gmail.com>
2019-03-07 20:44:26 +0300

    initial

--- AudioPlayer/ViewController.swift
+++ AudioPlayer/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
+import Foundation
 
 class ViewController: UIViewController {
 
+    let ebi = "https://d2uqwoe9jzxxtn.cloudfront.net/music/src/Ebi_Jane-Javani_1419049923.mp3"
+    
+    var player : AVPlayer?
+
+    // MARK: - Lifecycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // I'm Here...
     }
 
-
+    @IBAction func streamMusic(_ sender: Any) {
+        loadRadio(radioURL: ebi)
+    }
+    
+    @IBAction func playMusic(_ sender: Any) {
+        player?.play()
+    }
+    
+    @IBAction func pauseMusic(_ sender: Any) {
+        player?.pause()
+    }
+    
+    @IBAction func stopMusic(_ sender: Any) {
+        player?.pause()
+        guard let url = URL.init(string: ebi) else { return }
+        let playerItem = AVPlayerItem.init(url: url)
+        player = AVPlayer.init(playerItem: playerItem)
+    }
+    
+    func loadRadio(radioURL: String) {
+        guard let url = URL.init(string: radioURL) else { return }
+        let playerItem = AVPlayerItem.init(url: url)
+        player = AVPlayer.init(playerItem: playerItem)
+        player?.play()
+    }
 }
 
