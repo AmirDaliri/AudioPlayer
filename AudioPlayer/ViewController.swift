af48261cd1190c2f45d3fb3b754ed257d3a87b70
idaliri <idaliri69@gmail.com>
2019-03-07 18:47:03 +0300

    Initial Commit

---
+++ AudioPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioPlayer
+//
+//  Created by Amir Daliri on 7.03.2019.
+//  Copyright © 2019 AmirDaliri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
